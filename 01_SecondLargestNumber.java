c9583b2ea284048f593672833ae06a1c7c9fc0e8
Shivansh Gupta <shivanshgupta0987@gmail.com>
2024-11-17 16:16:52 +0530

    Day-1 of GFG160

---
+++ 01_SecondLargestNumber.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class SecondLargestNumber {
+
+    static int SecondLargest(int[] arr){
+        int n=arr.length;
+        int largest=arr[0];
+        int slargest=-1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>largest){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>slargest){
+                slargest = arr[i];
+            }
+        }
+        return slargest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println("The second largest is :- " + SecondLargest(arr));
+
+    }
+}
